2c0acbb3326956a4a99800aed11456e270be9004
Alireza Kamali <kamalialireza788@gmail.com>
2024-07-19 13:15:17 +0330

    handle ui about us page

--- app/(main)/about-us/page.tsx
+++ app/(main)/about-us/page.tsx
@@ -23,8 +23,8 @@ const AboutUs = () => {
                 </section>
             </div>
             <div className="w-full h-fit px-5 md:px-12 lg:px-28 py-16 space-y-16">
-                <div className="flex items-center gap-3 justify-between w-full">
-                    <div className="flex flex-col gap-2 max-w-lg">
+                <div className="flex items-center md:flex-row flex-col-reverse gap-x-10 gap-y-6 justify-between w-full">
+                    <div className="flex flex-col gap-2 md:max-w-[300px] lg:max-w-sm xl:max-w-lg">
                         <h3 className="text-3xl text-blue-500 font-bold">
                             About Company
                         </h3>
@@ -35,22 +35,22 @@ const AboutUs = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
                         </p>
                     </div>
-                    <div className="relative w-full max-w-[500px] h-[300px] overflow-hidden rounded-tr-[150px] rounded-bl-[130px] rounded-tl-[90px] rounded-br-[40px] shadow-[-25px_25px_0px_-15px_rgba(0,0,0,0.3)]">
+                    <div className="relative w-full max-w-[500px] md:h-[300px] h-[200px] overflow-hidden rounded-tr-[150px] rounded-bl-[130px] rounded-tl-[90px] rounded-br-[40px] shadow-[-25px_25px_0px_-15px_rgba(0,0,0,0.3)]">
                         <Image
                             src="/assets/company1.jpg"
                             alt="company"
                             fill
-                            className="size-full absolute filter brightness-90"
+                            className="size-full absolute filter brightness-90 object-cover"
                         />
                     </div>
                 </div>
-                <div className="flex items-center gap-3 justify-between w-full">
-                    <div className="relative w-full max-w-[500px] h-[300px] overflow-hidden rounded-tr-[100px] rounded-bl-[95px] rounded-tl-[120px] rounded-br-[60px] shadow-[-25px_25px_0px_-15px_rgba(0,0,0,0.3)]">
+                <div className="flex items-center md:flex-row flex-col gap-x-10 gap-y-6 justify-between w-full">
+                    <div className="relative w-full max-w-[500px] md:h-[300px] h-[200px] overflow-hidden rounded-tr-[100px] rounded-bl-[95px] rounded-tl-[120px] rounded-br-[60px] shadow-[-25px_25px_0px_-15px_rgba(0,0,0,0.3)]">
                         <Image
                             src="/assets/company2.jpg"
                             alt="company"
                             fill
-                            className="size-full absolute filter brightness-90"
+                            className="size-full absolute filter brightness-90 object-cover"
                         />
                     </div>
                     <div className="flex flex-col gap-2 max-w-lg">
